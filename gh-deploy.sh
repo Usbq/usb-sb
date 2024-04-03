22b18b796b69082723c226149be9a094772919dd
AshimeeAlt <161080149+AshimeeAlt@users.noreply.github.com>
2024-04-03 16:29:57 -0400

    Update gh-deploy.sh

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -2,10 +2,9 @@
 #!/usr/bin/env sh
 echo Initing
 git init
-# git checkout -B develop-builds
 git add .
 git commit -m 'Build Files'
 # same as the repository which started the github action
 git remote add origin https://github.com/Usbq/usb-sb
 git push -f
-echo Should have pushed
+git push origin develop-builds
